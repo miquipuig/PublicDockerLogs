4fdeed4fefe2ac86050ef35d188f8c7e0b033bc3
Ubuntu <ubuntu@ip-172-31-17-55.us-east-2.compute.internal>
2018-02-07 00:37:07 +0000

    cambios de pul

--- distribuir.php
+++ distribuir.php
@@ -1,16 +1,19 @@
 <?php
 
-echo "Eliminando imagenes<br/>";
+echo "Eliminando imagenes:<br/>";
 echo nl2br (shell_exec ( "ssh ubuntu@172.17.0.1 -i staging.pem docker rm -f intouch_web_1" ));
 echo nl2br (shell_exec ( "ssh ubuntu@172.17.0.1 -i staging.pem docker rm -f intouch_db_1" ));
+echo "<br/>";
 echo "Eliminando imagenes obsoletas:<br/>";
 echo nl2br (shell_exec ( "ssh ubuntu@172.17.0.1 -i staging.pem docker volume rm $(ssh ubuntu@172.17.0.1 -i staging.pem docker volume ls -qf dangling=true)" ));
-echo "Actualizando repositorio<br/>";
-echo  nl2br (shell_exec ( "ssh ubuntu@172.17.0.1 -i staging.pem git -C /home/ubuntu/intouch/ pull" ));
-echo "Compilando nuevas imágenes<br/>";
+echo "<br/>";
+echo "Actualizando repositorio:</br>";
+echo  nl2br (shell_exec ( "ssh ubuntu@172.17.0.1 -i staging.pem git -C /home/ubuntu/inTouch/ pull" ));
+echo "<br/>";
+echo "Compilando nuevas imagenes:<br/>";
 echo  nl2br (shell_exec ( "ssh ubuntu@172.17.0.1 -i staging.pem docker-compose -f /home/ubuntu/inTouch/docker-compose.yml build" ));
-
-echo "Arrancando nuevas imagenes<br/>";
+echo "<br/>";
+echo "Arrancando nuevas imagenes:<br/>";
 echo  nl2br (shell_exec ( "ssh ubuntu@172.17.0.1 -i staging.pem docker-compose -f /home/ubuntu/inTouch/docker-compose.yml up -d" ));
 echo "Imagenes arrancadas";
 ?>
